1f6b49bd2943b51d0f5d2c4aa7a15008021528fc
Nick Muller <muller_nicky@hotmail.com>
2019-08-07 12:19:46 +0200

    Initial checkin.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        // Update me and publish a new version of the app.
+        public string GetIdentifier() => "Test 1";
+    }
+}
